707b7645ad6c896d89c6c7dde109bc3355a4c137
Jarad Niemi <niemi@iastate.edu>
2012-09-26 14:14:47 -0500

    modified strateified.resample.R

--- R/stratified.resample.R
+++ R/stratified.resample.R
@@ -1,8 +1,28 @@
-stratified.resample <-
-function(weights,num.samples=length(weights)) {
-  lower.bound = (1:num.samples-1)/num.samples
-  uniforms    = runif(num.samples,lower.bound,lower.bound+1/num.samples)
 
-  return(inverse.cdf.weights(weights,uniforms))
+
+stratified.resample = function(weights, num.samples=length(weights), engine="R")
+{
+    check.weights(weights, log=F, normalized=T)
+    stopifnot(num.samples>0)
+
+    engine=pmatch(engine, c("R","C"))
+    n = length(weights)
+
+    switch(engine,
+    {
+        lbs = seq(0, by=1/num.samples, length=num.samples)
+        ubs = lbs+1/num.samples
+        u = runif(num.samples, lbs, ubs)
+        return(inverse.cdf.weights(weights,u,engine="R"))
+    },
+    {
+        # C implementation
+        out = .C("stratified_resample_R", 
+                 as.integer(n),
+                 as.double(weights),
+                 as.integer(num.samples),
+                 id = integer(num.samples))
+        return(out$id)
+    })
 }
 
