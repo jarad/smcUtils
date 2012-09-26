b28c41376b8cf684623f3037668f98de75bc0cb3
Jarad Niemi <niemi@iastate.edu>
2012-09-26 14:20:22 -0500

    added engine to branching.resample.R

--- R/branching.resample.R
+++ R/branching.resample.R
@@ -1,5 +1,7 @@
 branching.resample <-
-function(weights,num.samples=length(weights)) {
+function(weights,num.samples=length(weights), engine="R") {
+  if (engine!="R") stop("Only engine='R' implemented for branching.resample")
+
   expected.num.samples = weights*num.samples
   deterministic.reps   = floor(expected.num.samples)
   random.reps          = rbinom(num.samples,1,expected.num.samples-deterministic.reps)
