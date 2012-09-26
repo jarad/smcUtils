ca6b0539f6e8fbb567014d81cd0e49898c2536cd
Jarad Niemi <niemi@iastate.edu>
2012-09-26 14:55:03 -0500

    added check.weights.r

---
+++ R/check.weights.r
@@ -0,0 +1,20 @@
+
+check.weights = function(weights,log=F,normalized=T) 
+{
+    stopifnot(length(weights)>0)
+
+    if (all(log,normalized))
+        warning("logged and normalized weights are unusual")
+
+    if (!normalized) 
+    {
+        weights = renormalize(weights, log, "R") # returns unlogged
+        log=F
+    }
+
+    if (log) weights=exp(weights)
+
+    stopifnot(all(weights>=0))
+}
+
+
