4a0af2caaa75a41d9567d9dd21b5d1fb7d2d8f81
Jarad Niemi <niemi@iastate.edu>
2012-09-26 14:32:47 -0500

    added engine to residual.resample.R

--- R/residual.resample.R
+++ R/residual.resample.R
@@ -1,13 +1,38 @@
-residual.resample <-
-function(weights,num.samples=length(weights),resample.function=multinomial.resample) {
-  expected.num.samples = num.samples*weights
-  deterministic.reps   = floor(expected.num.samples)
-  sum.reps             = sum(deterministic.reps)
-  residual             = num.samples-sum.reps
 
-  deterministic.ids = rep2id(deterministic.reps)
-  random.ids        = resample.function((expected.num.samples-deterministic.reps)/residual,residual)
+residual.resample = function(weights, num.samples=length(weights), rrf="stratified", engine="R")
+{
+    rrf = pmatch(rrf,c("stratified","multinomial","systematic"))
+    if (is.na(rrf)) stop("No matching residual resampling function.")
 
-  return(c(deterministic.ids,random.ids))
+    check.weights(weights, log=F, normalized=T)
+    stopifnot(num.samples>0)
+
+    engine=pmatch(engine, c("R","C"))
+    n = length(weights)
+
+    switch(engine,
+    {
+        # R implementation
+        n.exp.samps = num.samples * weights
+        det.reps    = floor(n.exp.samps)
+        num.samples   = num.samples - sum(det.reps)
+        det.ids     = rep2id(det.reps, engine="R") 
+        weights     = renormalize(n.exp.samps-det.reps, engine="R")
+        switch(rrf,
+            { ran.ids = stratified.resample( weights, num.samples, engine="R") },
+            { ran.ids = multinomial.resample(weights, num.samples, engine="R") },
+            { ran.ids = systematic.resample( weights, num.samples, engine="R") })
+        return(c(det.ids, ran.ids))
+    },
+    {
+        # C implementation
+        out = .C("residual_resample_R", 
+                 as.integer(n),
+                 as.double(weights),
+                 as.integer(num.samples),
+                 id = integer(num.samples),
+                 as.integer(rrf))
+        return(out$id)
+    })
 }
 
