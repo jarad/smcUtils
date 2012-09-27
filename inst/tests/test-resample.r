02c8940e5c36817c7f489993ba64af0ea03c4bf7
Jarad Niemi <niemi@iastate.edu>
2012-09-27 08:53:01 -0500

    added Suggests: testthat

--- inst/tests/test-resample.r
+++ inst/tests/test-resample.r
@@ -1,108 +1,5 @@
 n.reps = 9
 
-context("Resampling utility functions")
-
-test_that("is.increasing works properly", {
-    inc = 1:3
-    expect_true(is.increasing(inc    ))
-    expect_true(is.increasing(inc,"R"))
-    expect_true(is.increasing(inc,"C"))
-
-    not = c(2,1,3)
-    expect_false(is.increasing(not    ))
-    expect_false(is.increasing(not,"R"))
-    expect_false(is.increasing(not,"C"))
-    
-    for (i in 1:n.reps) 
-    {
-        v <- rnorm(rpois(1,1)+3)
-        expect_identical(is.increasing(v,"R"), 
-                         is.increasing(v,"C"), 
-                         info=paste(v))
-    }
-})
-
-
-test_that("cusum works properly", {
-    v = 1:3
-    cs = c(1,3,6)
-    expect_equal(cusum(v    ),cs)
-    expect_equal(cusum(v,"R"),cs)
-    expect_equal(cusum(v,"C"),cs)
-
-    for (i in 1:n.reps) 
-    {
-        v = rnorm(rpois(1,10)+1)
-        expect_equal(cusum(v,"R"),
-                     cusum(v,"C"),
-                     info=paste(v))
-    }
-})
-
-
-test_that("rep2id works properly", {
-    rep = c(3,2,1)
-    id = c(0,0,0,1,1,2)
-    expect_equal(rep2id(rep    ),id)
-    expect_equal(rep2id(rep,"R")-1,id)
-    expect_equal(rep2id(rep,"C"),id)
-
-    for (i in 1:n.reps) 
-    {
-        rep = rpois(100,1)
-        expect_equal(rep2id(rep,"R")-1,
-                     rep2id(rep,"C"))
-    }
-})
-
-test_that("inverse.cdf.weights throws proper errors", {
-    u = numeric(0) 
-    expect_error(inverse.cdf.weights(w,u,"R"))
-    expect_error(inverse.cdf.weights(w,u,"C"))
-
-    u = runif(4); u[2] = -u[2]
-    expect_error(inverse.cdf.weights(w,u,"R"))
-    expect_error(inverse.cdf.weights(w,u,"C"))
-
-    u = runif(4)
-    w = numeric(0)
-    expect_error(inverse.cdf.weights(w,u,"R"))
-    expect_error(inverse.cdf.weights(w,u,"C"))
-
-    w = rep(1/4,4); w[3] = -w[3]
-    expect_error(inverse.cdf.weights(w,u,"R"))
-    expect_error(inverse.cdf.weights(w,u,"C"))
-})
-
-
-
-test_that("inverse.cdf.weights works properly", {
-    w = rep(1/4,4)
-    u = c(.1,.3,.6,.8)
-    id = 0:3
-    expect_equal(inverse.cdf.weights(w,u    )  ,id) 
-    expect_equal(inverse.cdf.weights(w,u,"R")-1,id)
-    expect_equal(inverse.cdf.weights(w,u,"C")  ,id)
-
-    u = c(.3,.1,.8,.6)
-    expect_equal(inverse.cdf.weights(w,u    )  ,id)
-    expect_equal(inverse.cdf.weights(w,u,"R")-1,id)
-    expect_equal(inverse.cdf.weights(w,u,"C")  ,id)
-
-
-    for (i in 1:n.reps) 
-    {
-        w = runif(rpois(1,10)+1)
-        w = w/sum(w)
-        u = runif(rpois(1,10)+1)
-        expect_equal(inverse.cdf.weights(w,u,"R")-1,
-                     inverse.cdf.weights(w,u,"C"))
-    }
-})
-
-
-
-
 
 ###############################################################
 # Effective sample size functions
@@ -111,87 +8,87 @@ test_that("inverse.cdf.weights works properly", {
 context("Sample size functions")
 
 
-test_that("ess throws proper errors", {
+test_that("ess.weights throws proper errors", {
     w = numeric(0)
-    expect_error(ess(w    ))
-    expect_error(ess(w,"R"))
-    expect_error(ess(w,"C"))
+    expect_error(ess.weights(w    ))
+    expect_error(ess.weights(w,"R"))
+    expect_error(ess.weights(w,"C"))
 
     w = runif(4); w[2] = -w[2]
-    expect_error(ess(w    ))
-    expect_error(ess(w,"R"))
-    expect_error(ess(w,"C"))
+    expect_error(ess.weights(w    ))
+    expect_error(ess.weights(w,"R"))
+    expect_error(ess.weights(w,"C"))
 })
 
-test_that("ess works properly", {
+test_that("ess.weights works properly", {
     n = 4
     w = rep(1/n, n)
-    expect_equal(ess(w,   ), n) 
-    expect_equal(ess(w,"R"), n) 
-    expect_equal(ess(w,"C"), n) 
+    expect_equal(ess.weights(w    ), n) 
+    expect_equal(ess.weights(w,"R"), n) 
+    expect_equal(ess.weights(w,"C"), n) 
 
     for (i in 1:n.reps) {
         w = runif(rpois(1,10)+1); w=w/sum(w)
-        expect_equal(ess(w,engine="R"),
-                     ess(w,engine="C"))
+        expect_equal(ess.weights(w,engine="R"),
+                     ess.weights(w,engine="C"))
     }
 
 })
 
 
-test_that("entropy throws proper errors", {
+test_that("ent.weights throws proper errors", {
     w = numeric(0)
-    expect_error(entropy(w    ))
-    expect_error(entropy(w,"R"))
-    expect_error(entropy(w,"C"))
+    expect_error(ent.weights(w    ))
+    expect_error(ent.weights(w,"R"))
+    expect_error(ent.weights(w,"C"))
 
     w = runif(4); w[2] = -w[2]
-    expect_error(entropy(w    ))
-    expect_error(entropy(w,"R"))
-    expect_error(entropy(w,"C"))
+    expect_error(ent.weights(w    ))
+    expect_error(ent.weights(w,"R"))
+    expect_error(ent.weights(w,"C"))
 })
 
-test_that("entropy works properly", {
+test_that("ent.weights works properly", {
     n = 4
     w = rep(1/n, n)
     ent = -log2(1/n)
-    expect_equal(entropy(w    ), ent) 
-    expect_equal(entropy(w,"R"), ent) 
-    expect_equal(entropy(w,"C"), ent) 
+    expect_equal(ent.weights(w    ), ent) 
+    expect_equal(ent.weights(w,"R"), ent) 
+    expect_equal(ent.weights(w,"C"), ent) 
 
     for (i in 1:n.reps) {
         w = runif(rpois(1,10)+1); w=w/sum(w)
-        expect_equal(entropy(w,engine="R"),
-                     entropy(w,engine="C"))
+        expect_equal(ent.weights(w,engine="R"),
+                     ent.weights(w,engine="C"))
     }
 
 })
 
 
-test_that("cov2 throws proper errors", {
+test_that("cov.weights throws proper errors", {
     w = numeric(0)
-    expect_error(cov2(w    ))
-    expect_error(cov2(w,"R"))
-    expect_error(cov2(w,"C"))
+    expect_error(cov.weights(w    ))
+    expect_error(cov.weights(w,"R"))
+    expect_error(cov.weights(w,"C"))
 
     w = runif(4); w[2] = -w[2]
-    expect_error(cov2(w    ))
-    expect_error(cov2(w,"R"))
-    expect_error(cov2(w,"C"))
+    expect_error(cov.weights(w    ))
+    expect_error(cov.weights(w,"R"))
+    expect_error(cov.weights(w,"C"))
 })
 
-test_that("cov2 works properly", {
+test_that("cov.weights works properly", {
     n = 4
     w = rep(1/n, n)
-    cov2 = 0
-    expect_equal(cov2(w    ), cov2) 
-    expect_equal(cov2(w,"R"), cov2) 
-    expect_equal(cov2(w,"C"), cov2) 
+    answer = 0
+    expect_equal(cov.weights(w    ), answer) 
+    expect_equal(cov.weights(w,"R"), answer) 
+    expect_equal(cov.weights(w,"C"), answer) 
 
     for (i in 1:n.reps) {
         w = runif(rpois(1,10)+1); w=w/sum(w)
-        expect_equal(cov2(w,engine="R"),
-                     cov2(w,engine="C"))
+        expect_equal(cov.weights(w,engine="R"),
+                     cov.weights(w,engine="C"))
     }
 
 })
@@ -333,7 +230,7 @@ test_that("residual resampling: R matches C", {
 test_that("resample chooses stratified resamping", {
     w = runif(4); w=w/sum(w)
     seed = proc.time()
-    set.seed(seed); m1 = resample(w, resampling.function="stratified")
+    set.seed(seed); m1 = resample(w, method="stratified")
     set.seed(seed); m2 = stratified.resample(w,engine="R")
     expect_equal(m1,m2)
 })
@@ -341,7 +238,7 @@ test_that("resample chooses stratified resamping", {
 test_that("resample chooses multinomial resamping", {
     w = runif(4); w=w/sum(w)
     seed = proc.time()
-    set.seed(seed); m1 = resample(w, resampling.function="multinomial")
+    set.seed(seed); m1 = resample(w, method="multinomial")
     set.seed(seed); m2 = multinomial.resample(w,engine="R")
     expect_equal(m1,m2)
 })
@@ -349,7 +246,7 @@ test_that("resample chooses multinomial resamping", {
 test_that("resample chooses systematic resamping", {
     w = runif(4); w=w/sum(w)
     seed = proc.time()
-    set.seed(seed); m1 = resample(w, resampling.function="systematic")
+    set.seed(seed); m1 = resample(w, method="systematic")
     set.seed(seed); m2 = systematic.resample(w,engine="R")
     expect_equal(m1,m2)
 })
@@ -357,7 +254,7 @@ test_that("resample chooses systematic resamping", {
 test_that("resample chooses residual resamping", {
     w = runif(4); w=w/sum(w)
     seed = proc.time()
-    set.seed(seed); m1 = resample(w, resampling.function="residual")
+    set.seed(seed); m1 = resample(w, method="residual")
     set.seed(seed); m2 = residual.resample(w,engine="R")
     expect_equal(m1,m2)
 })
