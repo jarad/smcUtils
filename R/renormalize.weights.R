86633bd6aaac408ef625b06d971e460bd02c520a
Jarad Niemi <niemi@iastate.edu>
2012-12-27 09:01:14 -0600

    changed renormalize.weights to renormalize

--- R/renormalize.weights.R
+++ R/renormalize.weights.R
@@ -1,24 +1,8 @@
 
 renormalize.weights = function(weights, log=FALSE, engine="R")
-{   
-    if (!log && any(weights<0)) stop("log=F but negative weights exist.")
+{  
+  .Deprecated("renormalize", package="smcUtils", old="renormalize.weights") 
 
-    engine=pmatch(engine, c("R","C"))
-
-    switch(engine,
-    {
-        # R implementation
-        if (log) weights = exp(weights - max(weights)) # max(weights) for numerical stability
-        return(weights/sum(weights))
-    },
-    {
-        # C implementation
-        n = length(weights)
-        out = .C("renormalize_R", 
-                 as.integer(n),
-                 as.integer(log),  
-                 weights=as.double(weights))
-        return(out$weights)
-    })   
+  renormalize(weights,log,engine)
 }
 
